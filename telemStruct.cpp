ee3f29b3a236d269ecfd309c30d679fc31705f50
Sam Chami <sc01914@gmail.com>
2018-02-06 20:10:12 -0800

    Added variables with proper storage types

--- telemStruct.cpp
+++ telemStruct.cpp
@@ -1,18 +1,27 @@
 /**
 Data Outputs and Data Types required by SpaceX
 
-team_id = UINT8
-Status = UINT8
-Acceleration = INT32
-Position = INT32
-Velocity = INT32
-battery_voltage = INT32
-battery_current = INT32
-battery_temperature = INT32
-stripe_count = UINT32
+  team_id = UINT8
+  Status = UINT8
+  Acceleration = INT32
+  Position = INT32
+  Velocity = INT32
+  battery_voltage = INT32
+  battery_current = INT32
+  battery_temperature = INT32
+  stripe_count = UINT32
 **/
 
 struct telemStruct {
+  unsigned char	team_id = 0;
+  unsigned char	status = 0;
 
+  signed long int acceleration = 0;
+  signed long int position = 0;
+  signed long int velocity = 0;
+  signed long int battery_voltage = 0;
+  signed long int battery_current = 0;
+  signed long int battery_temperature = 0;
 
+  unsigned long int stripe_count = 0;
 };
