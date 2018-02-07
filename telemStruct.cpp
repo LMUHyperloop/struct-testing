35198af722767215dfd556543e86530acee9a81b
Sam Chami <sc01914@gmail.com>
2018-02-06 20:00:03 -0800

    Added struct file

---
+++ telemStruct.cpp
@@ -0,0 +1,18 @@
+/**
+Data Outputs and Data Types required by SpaceX
+
+team_id = UINT8
+Status = UINT8
+Acceleration = INT32
+Position = INT32
+Velocity = INT32
+battery_voltage = INT32
+battery_current = INT32
+battery_temperature = INT32
+stripe_count = UINT32
+**/
+
+struct telemStruct {
+
+
+};
